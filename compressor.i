11ba6937973c7e9fd92077eea7ee2f190f9633dc
Dodge W. Coates <dcoates91@gmail.com>
2016-06-20 23:50:47 -0400

    Added compressor core

--- compressor.i
+++ compressor.i
@@ -1,2 +1,4 @@
 %module compressor
--extern int basic(const char* str, int str_pos, int N);
+
+extern int basic(const char* str, int str_pos, int N);
+extern const char* core(const char* str, const char* alphabet, const int comp_fact, const int N);
--- compressormodule.c
+++ compressormodule.c
@@ -1,6 +1,11 @@
 #include <string.h>
+#include <stdlib.h>
 
 #define min(a,b) (((a) < (b)) ? (a) : (b))
+
+int output_char(char* output, const int x, const char* alphabet, const int A, const int C);
+int basic(const char* str, const int str_pos, const int N);
+
 
 const char* core(const char* str, const char* alphabet, const int comp_fact, const int N) {
   int str_pos = 0,
@@ -9,55 +14,64 @@ const char* core(const char* str, const char* alphabet, const int comp_fact, con
       indx,
       acc;
 
-  char* output = ;
+  // this is unsafe, particularly for small input strings, but whatever.
+  int max_sig_len = (str_len/comp_fact) * 1.5;
+  int sig_len = 0;
+  char* output = malloc(max_sig_len);
 
-  // Accumulate a value for each position that included an entire
-  // neighborhood.
   for(str_pos=0; (str_pos+N) < str_len; str_pos++) {
     acc = 0;
-    // Starting with no bits set in an accumulator, for each element in;
-    // the neighborhood, XOR in the element at a fresh 8-bit position.;
-    // Wrap around and keep going if N is long enough to exhaust the 64;
-    // bits in a long.;
-    acc = binascii.crc32(string[str_pos:str_pos + self.N]) + 2**32;
-    if((acc % comp_fact) == 0) {
-      indx = acc % alpha_len;
-      out_char = self.get_alphabet()[indx];
-      charlist.append(out_char);
+
+    // whoops...
+
+    int char_outputted = output_char(output, sig_len, alphabet, acc, comp_fact);
+    if(char_outputted) {
+      sig_len++;
     }
-  }
-
-  /*
-   * Return a character from 'alphabet' pseudorandomly. Likelihook of returning a character is 1/C.
-   */
-char output_char(const char* alphabet, const int A, const int C) {
-  char output = '';
-
-
-  if((A % C) == 0) {
-    indx = A % alpha_len;
-    output = alphabet[indx]
   }
 
   return output;
 }
 
-  return ''.join(charlist);
-}
-int basic(const char* str, const int str_pos, const int N) {;
-  int acc = 0,
-    val,
-    i;
 
-  int end_pos;
-  end_pos = min(str_pos+N, strlen(str));
+/*
+ * Assign pseudorandom character from 'alphabet' at position 'x' in string 'output'.
+ * Likelihood of assigning a character must be near 1/C assuming a uniform distribution of
+ * input 'A'.
+ */
+int output_char(char* output, const int x, const char* alphabet, const int A, const int C) {
+  if((A % C) == 0) {
+    indx = A % alpha_len;
+    output[x] = alphabet[indx];
 
-  for(i=str_pos; i<end_pos; i++) {
-    val = (int) str[i];
-    val <<= ((i-str_pos) * 8 % 56);
-    acc ^= val;
-    acc = abs(acc);
+    return 1;
   }
 
-  return acc;
+  return 0;
 }
+
+
+/*
+ * Compress the string between str_pos and str_pos+N into a 64-bit value
+ * with a near-uniform distribution of randomness.
+ */
+ int basic(const char* str, const int str_pos, const int N) {;
+   int acc = 0,
+     val,
+     i,
+     end_pos;
+
+   end_pos = min(str_pos+N, strlen(str));
+   // Starting with no bits set in an accumulator, for each element in
+   // the neighborhood str_pos to end_pos, XOR in the element at a fresh 8-bit
+   // position. Wrap around and keep going if N is long enough to exhaust the 56
+   // bits alloted.
+   for(i=str_pos; i<end_pos; i++) {
+     val = (int) str[i];
+     val <<= ((i-str_pos) * 8 % 56);
+     acc ^= val;
+     acc = abs(acc);
+   }
+
+   return acc;
+ }
